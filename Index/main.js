e22914daf60cd65550841854db81962dc758017e
Zurash <zurash924@gmail.com>
2024-07-26 22:35:25 +0500

    Add files via upload

---
+++ Index/main.js
@@ -0,0 +1,33 @@
+console.log("Hareem Asif");
+var myname = "Hareem";
+console.log(myname);
+var myname1 = "a";
+myname = "A";
+//const
+var myfriend = "Ali";
+var myfriend1 = "A";
+//var
+var myage = 5;
+var myage = 10;
+myage = 19;
+//number 
+var decimal = 6;
+//string 
+var color = "blue";
+//array 
+var list = [1, 2, 3,];
+//tuple
+var x;
+x = ["hello", 10];
+//any 
+var notSure = 76;
+//null & undefined
+var u = undefined;
+var n = null;
+//object 
+var obj;
+console.log("Hareem Asif");
+console.log("Noor");
+console.log("myname4");
+var myname4 = "Hareem";
+myname4 = "Ali";
---
+++ Index/main.ts
@@ -0,0 +1,133 @@
+console . log ("Hareem Asif");
+let myname = "Hareem"
+console . log (myname)
+let myname1 = "a"
+myname = "A"
+//const
+const myfriend = "Ali"
+const myfriend1 = "A"
+//var
+var myage = 5
+var myage = 10 
+myage = 19 
+//number 
+let decimal : number = 6;
+//string 
+let color : string = "blue";
+//array 
+let list: number [] = [1, 2, 3,];
+//tuple
+let x: [string, number]; x = ["hello", 10];
+//any 
+let notSure: any = 76;
+//null & undefined
+let u: undefined = undefined;
+let n: null = null;
+//object 
+let obj: { name: "Noor" };
+console . log ("Hareem Asif");
+console . log ("Noor");
+console . log ("myname4");
+let myname4 = "Hareem"
+myname4 = "Ali"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
